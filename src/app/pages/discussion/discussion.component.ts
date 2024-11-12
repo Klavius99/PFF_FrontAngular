5bc6109856f10949c88bde82c3debd7eadbb8705
Klavius99 <ndarawpro@gmail.com>
2024-11-12 00:28:34 +0000

    switch user and group discussion with buton

--- src/app/pages/discussion/discussion.component.ts
+++ src/app/pages/discussion/discussion.component.ts
@@ -2,14 +2,26 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../../shared/navbar/navbar.component";
 import { AppComponent } from '../../app.component';
 import { CommonModule } from '@angular/common';
+import { MessageLayoutComponent } from "../../shared/message-layout/message-layout.component";
+import { UsersDiscussionListComponent } from "../../shared/users-discussion-list/users-discussion-list.component";
+import { GroupsDiscussionListComponent } from '../../shared/groups-discussion-list/groups-discussion-list.component';
 
 @Component({
   selector: 'app-discussion',
   standalone: true,
-  imports: [NavbarComponent,CommonModule],
+  imports: [NavbarComponent, CommonModule, MessageLayoutComponent, UsersDiscussionListComponent, GroupsDiscussionListComponent],
   templateUrl: './discussion.component.html',
   styleUrl: './discussion.component.css'
 })
 export class DiscussionComponent {
-    
+  selectedView: 'users' | 'groups' = 'users'; // Définit 'users' comme vue par défaut
+
+  // Méthodes pour changer la vue
+  showUsers() {
+    this.selectedView = 'users';
+  }
+
+  showGroups() {
+    this.selectedView = 'groups';
+  }
 }
